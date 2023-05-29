c1cb839331fa888243a86bf91ba66aa421fc6777
Torres5271 <gustavo.souza@sptech.school>
2023-05-29 14:26:40 -0300

    alteração banco

--- script_projetoIndividual.sql
+++ script_projetoIndividual.sql
@@ -1,40 +1,36 @@
 drop database projetoIndividual;
- create database projetoIndividual;
 
+create database projetoIndividual;
 
 use projetoIndividual;
 
-create table cadastro(
-idCadastro int primary key auto_increment,
-usuario varchar(45),
-email varchar(45),
-senha varchar(45),
-confirmarSenha varchar(45)
-);
-desc cadastro;
-select * from cadastro;
-
-
-
-create table analytics(
-idAnalytics int auto_increment,
-shanks int,
-garp int,
-ace int,
-outros int,
-fkCadastro int, constraint fkCad foreign key (fkCadastro) references cadastro(idCadastro),
-			constraint pkLogAna primary key (idAnalytics, fkCadastro)
-);
-desc analytics;
-select * from analytics;
-
 create table personagem(
 idPersonagem int primary key auto_increment,
-nome varchar(45),
-dtNascimento date,
-funcao varchar(45),
-bando varchar(45),
-fkCadPers int, constraint fkCadPers foreign key (fkCadPers) references cadastro(idCadastro)
+nomePersonagem varchar(45)
 );
-desc personagem;
+
+insert into personagem(nomePersonagem) values
+('luffy'),
+('zoro'),
+('nami'),
+('usopp'),
+('sanji'),
+('chopper'),
+('robin'),
+('franky'),
+('brook'),
+('jinbe');
+
 select * from personagem;
+
+create table usuario(
+idUsuario int primary key auto_increment,
+nome varchar(45),
+email varchar(45),
+senha varchar(45),
+fkPersonagem int, constraint fkPersonagem foreign key (fkPersonagem) references personagem(idPersonagem)
+);
+
+select * from usuario;
+
+
